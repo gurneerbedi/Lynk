6f404351d0379aeb4bace25bab1eb31170b11700
Gurneer <gurneer1223@gmail.com>
2025-07-17 21:41:08 -0400

    added sign in page

--- app/landing.tsx
+++ app/landing.tsx
@@ -1,24 +1,53 @@
-import { StyleSheet } from "react-native";
-import { H1 } from "@/components/Heading";
+import { StyleSheet, Text } from "react-native";
+import { H1 , H3} from "@/components/Heading";
 import { GoogleSigninButton } from "@react-native-google-signin/google-signin";
 import Page from "@/components/Page";
 import { useUser } from "@/UserContext";
 
-export default function Index() {
+export default function Login() {
   const { signIn } = useUser();
 
   return (
-    <Page style={styles.center}>
-      <H1>Welcome to the app</H1>
-      <GoogleSigninButton size={GoogleSigninButton.Size.Wide} color={GoogleSigninButton.Color.Dark} onPress={signIn} />
+    <Page style={styles.container}>
+      <H3 style={styles.welcome}>Welcome Neighbour! 👋</H3>
+      <H1 style={styles.appName}>Lynk</H1>
+      <H3 style={styles.welcome}>
+        Help is just next door
+      </H3>
+
+      <GoogleSigninButton
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Light} 
+        onPress={signIn}
+        style={styles.googleButton}
+      />
     </Page>
   );
 }
 
 const styles = StyleSheet.create({
-  center: {
+  container: {
+    flex: 1,
+    backgroundColor: "#E6E0F8", // soft lavender background
     justifyContent: "center",
     alignItems: "center",
-    gap: 16,
+    paddingHorizontal: 24,
+  },
+  welcome: {
+    fontSize: 22,
+    color: "#5D4B8C", // deep purple text
+    marginBottom: 8,
+    fontWeight: "600",
+  },
+  appName: {
+    fontSize: 60,
+    color: "#7B5ACF", // medium purple for Lynk
+    fontWeight: "900",
+    marginBottom: 6,
+  },
+
+  googleButton: {
+    borderRadius: 10,
+    elevation: 10, 
   },
 });
